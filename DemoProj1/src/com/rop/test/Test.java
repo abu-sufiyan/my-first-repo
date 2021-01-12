25833d5963c48a66165ccbf9aef8ae0ccd04c077
Dev2 <sufiyanrafey@gmail.com>
2021-01-13 01:19:13 +0530

    Dev1 implement Test.java

--- DemoProj1/src/com/rop/test/Test.java
+++ DemoProj1/src/com/rop/test/Test.java
@@ -1,5 +1,10 @@
 package com.rop.test;
 
-public class Test {
+import com.rop.beans.Utility;
 
+public class Test {
+public static void main(String[] args) {
+	Utility u=new Utility();
+	System.out.println(u.getSysDate());
+}
 }
